2b987e4968b107da1c0acae3f6a0760ae4d8569e
Markus Knutson Barstad <markus@Markuss-MacBook-Air.local>
2026-04-23 13:29:28 +0200

    Added loading and yAxis title

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css'
-import { Chart, Title, XAxis } from "@highcharts/react";
+import { Chart, Title, XAxis, YAxis } from "@highcharts/react";
 import { LineSeries } from "@highcharts/react/series/Line";
 
 interface ChartData {
@@ -10,6 +10,7 @@ interface ChartData {
 
 function App() {
   const [chartData, setChartData] = useState<ChartData | null>(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     fetch('https://data.ssb.no/api/pxwebapi/v2/tables/07221/data?lang=en&valueCodes[Region]=TOTAL&valueCodes[Boligtype]=00&valueCodes[ContentsCode]=Boligindeks&valueCodes[Tid]=*&outputformat=json-stat2')
@@ -18,15 +19,22 @@ function App() {
         const labels = Object.keys(data.dimension.Tid.category.label)
         const values = data.value
         setChartData({ labels, values })
+        setLoading(false)
       })
   }, [])
-  return (
-    <Chart>
-      <Title>Norwegian House Price Index</Title>
-      <XAxis categories={chartData ? chartData.labels : []} />
-      <LineSeries data={chartData ? chartData.values : [null]} />
-    </Chart>
-  )
+
+return (
+  <>
+    {loading ? <p>Loading...</p> : (
+      <Chart>
+        <Title>Norwegian House Price Index</Title>
+        <XAxis categories={chartData?.labels ?? []} />
+        <YAxis title={{ text: 'Index (2015=100)' }} />
+        <LineSeries data={chartData?.values ?? [null]} />
+      </Chart>
+    )}
+  </>
+)
 }
 
 export default App
