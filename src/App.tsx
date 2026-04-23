16c9c57022544a01c797c4cb25d2cc4d3922569a
Markus Knutson Barstad <markus@Markuss-MacBook-Air.local>
2026-04-23 13:16:34 +0200

    Added hc and ssb call

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import { Chart, Title, XAxis } from "@highcharts/react";
+import { LineSeries } from "@highcharts/react/series/Line";
+
+interface ChartData {
+  labels: string[]
+  values: number[]
+}
 
 function App() {
+  const [chartData, setChartData] = useState<ChartData | null>(null)
+
+  useEffect(() => {
+    fetch('https://data.ssb.no/api/pxwebapi/v2/tables/07221/data?lang=en&valueCodes[Region]=TOTAL&valueCodes[Boligtype]=00&valueCodes[ContentsCode]=Boligindeks&valueCodes[Tid]=*&outputformat=json-stat2')
+      .then(res => res.json())
+      .then(data => {
+        const labels = Object.keys(data.dimension.Tid.category.label)
+        const values = data.value
+        setChartData({ labels, values })
+      })
+  }, [])
   return (
-    <>
-      <div>wowowoowowo</div>
-    </>
+    <Chart>
+      <Title>Norwegian House Price Index</Title>
+      <XAxis categories={chartData ? chartData.labels : []} />
+      <LineSeries data={chartData ? chartData.values : [null]} />
+    </Chart>
   )
 }
 
