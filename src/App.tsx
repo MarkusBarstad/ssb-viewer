3cb9814ffe578509392083ed8878b5266f02869b
Markus Knutson Barstad <markus@Markuss-MacBook-Air.local>
2026-04-23 13:48:46 +0200

    Added region selection

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,37 @@ interface ChartData {
 function App() {
   const [chartData, setChartData] = useState<ChartData | null>(null)
   const [loading, setLoading] = useState(true)
+  const [selectedRegion, setSelectedRegion] = useState("TOTAL")
 
   useEffect(() => {
-    fetch('https://data.ssb.no/api/pxwebapi/v2/tables/07221/data?lang=en&valueCodes[Region]=TOTAL&valueCodes[Boligtype]=00&valueCodes[ContentsCode]=Boligindeks&valueCodes[Tid]=*&outputformat=json-stat2')
+    setLoading(true)
+
+    fetch(`https://data.ssb.no/api/pxwebapi/v2/tables/07221/data?lang=en&valueCodes[Region]=${selectedRegion}&valueCodes[Boligtype]=00&valueCodes[ContentsCode]=Boligindeks&valueCodes[Tid]=*&outputformat=json-stat2`)
       .then(res => res.json())
       .then(data => {
         const labels = Object.keys(data.dimension.Tid.category.label)
-        const values = data.value
+        const values = data.value.map((v: number) => v === 0 ? null : v)
         setChartData({ labels, values })
         setLoading(false)
       })
-  }, [])
+  }, [selectedRegion])
 
 return (
   <>
     {loading ? <p>Loading...</p> : (
+      <>
       <Chart>
         <Title>Norwegian House Price Index</Title>
         <XAxis categories={chartData?.labels ?? []} />
         <YAxis title={{ text: 'Index (2015=100)' }} />
         <LineSeries data={chartData?.values ?? [null]} />
       </Chart>
+      <select value={selectedRegion} onChange={e => setSelectedRegion(e.target.value)}>
+        <option value="TOTAL">The whole country</option>
+        <option value="001">Oslo including Bærum</option>
+        <option value="003">Bergen</option>
+      </select>
+      </>
     )}
   </>
 )
